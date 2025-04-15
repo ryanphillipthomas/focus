5fdde67694b054c519ceb248f4e225785cb4c0fb
ryan.thomas <6352500+ryanphillipthomas@users.noreply.github.com>
2025-04-15 09:23:13 -0400

    Re-Indent

--- Focus/Focus/SupportCode/Settings/SettingsView.swift
+++ Focus/Focus/SupportCode/Settings/SettingsView.swift
@@ -36,79 +36,79 @@ struct SettingsView: View {
 
     var body: some View {
         Form {
-        Section(header: Text(LocalizedStringResource("support_code_library_version"))) {
+            Section(header: Text(LocalizedStringResource("support_code_library_version"))) {
                 NavigationLink(destination: AnalyticsSettingsListView()) {
                     Label("Analytics", systemImage: "gear")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_analytics")
                 })
-
+                
                 NavigationLink(destination: CalendarSettingsListView(calendarManager: calendarManager, calendarViewModel: calendarViewModel)) {
                     Label("Calendar", systemImage: "calendar")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_authentication")
                 })
-
+                
                 NavigationLink(destination: AuthenticationListView(auth: auth)) {
                     Label("Authentication", systemImage: "lock")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_authentication")
                 })
-
+                
                 NavigationLink(destination: HealthStatsListView()) {
                     Label("Health", systemImage: "heart")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_refresh_health")
                 })
-
+                
                 NavigationLink(destination: MusicListView(musicManager: musicManager)) {
                     Label("iCloud", systemImage: "icloud")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_refresh_icloud")
                 })
-
+                
                 NavigationLink(destination: MusicListView(musicManager: musicManager)) {
                     Label("Music", systemImage: "music.quarternote.3")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_connect_apple_music")
                 })
-
+                
                 NavigationLink(destination: NotificationTestView()) {
                     Label("Push Notifications", systemImage: "bell")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_manage_notifications")
                 })
-
+                
                 NavigationLink(destination: OnboardingListView(hasCompletedOnboarding: $hasCompletedOnboarding)) {
                     Label("Onboarding", systemImage: "chart.line.text.clipboard")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_onboarding")
                 })
-
+                
                 NavigationLink(destination: RemindersListView(reminderManager:reminderManager)) {
                     Label("Reminders", systemImage: "checklist")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_reminders")
                 })
-
+                
                 NavigationLink(destination: SubscriptionListView(viewModel: SubscriptionViewModel(mock: true))) {
                     Label("Subscriptions", systemImage: "dollarsign.arrow.trianglehead.counterclockwise.rotate.90")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_upgrade_to_pro")
                 })
-
+                
                 NavigationLink(destination: ThemeSelectionView()) {
                     Label("Theme", systemImage: "paintpalette")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_customize")
                 })
-            
+                
                 NavigationLink(destination: CacheSettingsListView()) {
                     Label("Cache", systemImage: "externaldrive")
                 }.simultaneousGesture(TapGesture().onEnded {
                     AnalyticsManager.shared.logEvent("settings_selection_cache")
                 })
-            
+                
                 NavigationLink(destination: AppReviewListView()) {
                     Label("Review App", systemImage: "star")
                 }.simultaneousGesture(TapGesture().onEnded {
